f436bc198279cdf7a56348f74fc6d3b35f2ae559
DanielRTato <drodrigueztato@danielcastelao.org>
2025-11-09 22:10:50 +0100

    Añadir clase de datos y enums para el juego

--- app/src/main/java/com/SarayDani/sidi/Datos.kt
+++ app/src/main/java/com/SarayDani/sidi/Datos.kt
@@ -1,4 +1,27 @@
 package com.SarayDani.sidi
 
-class Datos {
+import androidx.compose.ui.graphics.Color
+
+data class Datos (
+    var ronda: Int = 0,                       // Número de ronda actual
+    var secuencia: MutableList<Int> = mutableListOf(),  // Secuencia generada por el juego
+    var secuenciaIntroducida: MutableList<Int> = mutableListOf(), // Secuencia del jugador
+    var record: Int = 0,                      // Máximo número de rondas superadas
+    var estados: Estados = Estados.Inicio,    // Estado actual del juego
+)
+
+enum class Colores(val color: Color, val txt: String) {
+    CLASE_ROJO(color = Color.Red, txt = "rojo"),
+    CLASE_VERDE(color = Color.Green, txt = "verde"),
+    CLASE_AZUL(color = Color.Blue, txt = "azul"),
+    CLASE_AMARILLO(color = Color.Yellow, txt = "amarillo"),
+    CLASE_START(color = Color.Magenta, txt = "Start")
+}
+
+enum class Estados {
+    Inicio,
+    GenerarSecuencia,
+    IntroducirSecuencia,
+    GameOver,
+    Pausa
 }
