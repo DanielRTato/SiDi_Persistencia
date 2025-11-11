9c56cc59c543960866ed5b4d30d05ae34be19ef4
SGRuzo <sgarciaruzo@danielcastelao.org>
2025-11-11 12:46:04 +0100

    Ajustes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.SarayDani.sidi"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.SarayDani.sidi"
